ce429b0077a122836437fd8c77dac46bcf866ed7
dhew0016 <dhew0016@student.monash.edu>
2025-09-27 20:05:28 +1000

    Added html and js files

---
+++ js/vega_lite_viz.js
@@ -0,0 +1,4 @@
+var spec = "js/venue_avg_score.json";
+vegaEmbed('#map', spec).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+})
