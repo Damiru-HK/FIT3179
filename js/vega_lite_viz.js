39e5b38ded7048320afe42325ffea0c2ad375804
dhew0016 <dhew0016@student.monash.edu>
2025-10-11 20:29:39 +1100

    created web page using html and css, added visualisations using javascript

--- js/vega_lite_viz.js
+++ js/vega_lite_viz.js
@@ -1,4 +1,24 @@
-var spec = "js/venue_avg_score.json";
-vegaEmbed('#map', spec).then(function(result){
+var spec_history = "js/history_viz.json";
+vegaEmbed('#history_viz', spec_history).then(function(result){
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-})
+}).catch(console.error)
+
+var spec_map = "js/venue_avg_score.json";
+vegaEmbed('#map_viz', spec_map).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var spec_dotplot = "js/socre_comparison_viz.json";
+vegaEmbed('#dotplot_viz', spec_dotplot).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var spec_bar = "js/batting_advantage.json";
+vegaEmbed('#bar_viz', spec_bar).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var spec_final = "js/critical_matches.json";
+vegaEmbed('#final_viz', spec_final).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
